e3ee45774033f3f03fe0365d586a249212077721
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-08-22 08:59:39 -0400

    Add new class and MarcScene.

---
+++ Assets/Class/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager{
+
+    public int Score { get; set; }
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/DetectionSonore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectionSonore : MonoBehaviour {
+
+    //nb de cube que les ennemis entendes 
+    public int distanceEntendu;
+
+    private Rigidbody player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Rigidbody>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LateUpdate() {
+        Physics.OverlapSphere(player.transform.position, distanceEntendu);
+    
+    }
+}
