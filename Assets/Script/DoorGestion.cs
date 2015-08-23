02ebbd4d65f2edae492c47765f43ec825ab21d41
Marc-Antoine Dumont <dumont.marcantoine13@gmail.com>
2015-08-22 22:06:47 -0400

    Add DoorGestion

---
+++ Assets/Script/DoorGestion.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorGestion : MonoBehaviour {
+
+    private Rigidbody _rigidbody;
+    private bool doorIsOpen;
+    private SoundManager soundManager;
+
+    //public Transform penture;
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+        doorIsOpen = false;
+        soundManager = SoundManager.getInstance();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player")
+        {
+            if (doorIsOpen) {
+                CloseDoor();
+            }
+            else
+            {
+                OpenDoor();                
+            }            
+        }
+
+    }
+
+    public void OpenDoor()
+    {
+        _rigidbody.transform.Rotate(new Vector3(0, _rigidbody.transform.position.y, 0), -90);
+        doorIsOpen = true;
+        soundManager.StartOpenDoorSound();
+    }
+
+    public void CloseDoor()
+    {
+        _rigidbody.transform.Rotate(new Vector3(0, _rigidbody.transform.position.y, 0), 90);
+        doorIsOpen = false;
+        soundManager.StartCloseDoorSound();
+    }
+
+
+}
